8bf476a62ee09fd2e923e843a840bdbc67a09934
Nicolas Suarez <nicolasdavidsuarez@gmail.com>
2026-04-07 17:03:40 -0300

    ultima antes e clases 07/04/26
    ultima antes e clases 07/04/26

--- Source/NecroLife/Private/Widgets/NecroLifeHud.cpp
+++ Source/NecroLife/Private/Widgets/NecroLifeHud.cpp
@@ -3,7 +3,9 @@
 
 #include "Widgets/NecroLifeHud.h"
 
+#include "NecroLifePlayerState.h"
 #include "Components/AttributeComponent.h"
+#include "Components/InventoryComponent.h"
 #include "Components/ProgressBar.h"
 #include "Components/TextBlock.h"
 #include "Components/UHealthComponent.h"
@@ -11,7 +13,7 @@
 void UNecroLifeHud::NativeConstruct()
 {
 	Super::NativeConstruct();
-	// 1. Obtenemos el Pawn (tu BP_Character)
+
 	APawn* PlayerPawn = GetOwningPlayerPawn();
 	if (!PlayerPawn) return;
 
@@ -20,26 +22,18 @@ void UNecroLifeHud::NativeConstruct()
 
 	if (HealthComp)
 	{
-		// 3. ¡EL BINDEO! 
-		// Primero removemos por seguridad para no binear dos veces si el widget se reconstruye
-		//HealthComp->OnHealthChanged.RemoveDynamic(this, &UNecroLifeHUD::HandleHealthChanged);
-        
-		// Ahora sí, agregamos el bindeo
 		HealthComp->OnHealthChanged.AddDynamic(this, &UNecroLifeHud::HandleHealthChanged);
-        
-		// Tip: Llamá a la función una vez manualmente para inicializar la barra con la vida actual
-		// HandleHealthChanged(HealthComp->GetCurrentHealth(), HealthComp->GetMaxHealth());
+
 	}
 	UAttributeComponent* Attributes = PlayerPawn->FindComponentByClass<UAttributeComponent>();
 
 	if (Attributes)
 	{
-		// Bindeamos el evento
+
 		Attributes->OnXPChanged.AddDynamic(this, &UNecroLifeHud::HandleXPChanged);
-            
-		// Llamada inicial para que la barra no empiece vacía
-		// HandleXPChanged(Attributes->GetXP(), Attributes->GetNextLevelXP(), Attributes->GetLevel());
+
 	}
+	
 }
 
 void UNecroLifeHud::HandleHealthChanged(float CurrentHealth, float MaxHealth)
@@ -52,7 +46,7 @@ void UNecroLifeHud::HandleHealthChanged(float CurrentHealth, float MaxHealth)
 
 void UNecroLifeHud::HandleXPChanged(float CurrentXP, float XPToNextLevel, int32 CurrentLevel)
 {
-	// Esto reemplaza los nodos de división y Append del Blueprint
+	
 	if (XPBar && XPToNextLevel > 0.0f)
 	{
 		float Percent = CurrentXP / XPToNextLevel;
@@ -61,9 +55,33 @@ void UNecroLifeHud::HandleXPChanged(float CurrentXP, float XPToNextLevel, int32 
 
 	if (LevelText)
 	{
-		// Reemplaza el "Append" de "Nivel: " + Valor
 		FString LevelString = FString::Printf(TEXT("Nivel: %d"), CurrentLevel);
 		LevelText->SetText(FText::FromString(LevelString));
 		
+	}
+}
+
+void UNecroLifeHud::ActualizarInventario(const TArray<UItemData*>& ItemsRecibidos)
+{
+
+	if (!bBindeo)
+	{
+		ANecroLifePlayerState* MyPS = Cast<ANecroLifePlayerState>(GetOwningPlayerState());
+		if (MyPS )
+		{
+			
+			UInventoryComponent* Inventory= MyPS->FindComponentByClass<UInventoryComponent>();
+			if (Inventory)
+			{
+				Inventory->OnShowItem.AddDynamic(this, &UNecroLifeHud::ActualizarInventario);
+				bBindeo = true;
+			}
+			
+		}
+		GEngine->AddOnScreenDebugMessage(-1,5.0f, FColor::Red, "ActualizarInventario pero no bindeo");	
+
+	}else
+	{		
+		BP_UpdateInventoryUI(ItemsRecibidos);	
 	}
 }
