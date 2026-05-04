b1cfa66f392e67dc97a939a9caa3be71d0ae43f4
Guido p <guidoparisi91@gmail.com>
2026-05-04 19:31:24 -0300

    cambios basicos en el personaje enemiy ranged

--- Source/NecroLife/NecroLifeCharacter.cpp
+++ Source/NecroLife/NecroLifeCharacter.cpp
@@ -50,7 +50,7 @@ ANecroLifeCharacter::ANecroLifeCharacter()
    GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
    GetCharacterMovement()->JumpZVelocity = 500.f;
    GetCharacterMovement()->AirControl = 0.35f;
-   GetCharacterMovement()->MaxWalkSpeed = 500.f;
+   GetCharacterMovement()->MaxWalkSpeed = 50.f;
    GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
    GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
    GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
